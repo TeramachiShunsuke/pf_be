e9133beccb6e74ed361cb2c8363f5cadb8239321
ShunsukeTeramachi <like.football.17@gmail.com>
2021-07-06 01:27:09 +0900

    at first

---
+++ api/user/views.py
@@ -0,0 +1,67 @@
+from django.contrib.auth import authenticate
+from django.db import transaction
+from django.http import HttpResponse, Http404
+from rest_framework import  permissions, generics
+from rest_framework_jwt.settings import api_settings
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.response import Response
+from rest_framework import status, viewsets, filters
+from rest_framework.views import APIView
+from .serializer import UserSerializer
+from .models import User, UserManager
+
+# ユーザ作成のView(POST)
+class AuthRegister(generics.CreateAPIView):
+    permission_classes = (permissions.AllowAny,)
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    @transaction.atomic
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# ユーザ情報取得のView(GET)
+class AuthInfoGetView(generics.RetrieveAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get(self, request, format=None):
+        return Response(data={
+            'username': request.user.username,
+            'email': request.user.email,
+            'profile': request.user.profile,
+            },
+            status=status.HTTP_200_OK)
+
+# ユーザ情報更新のView(PUT)
+class AuthInfoUpdateView(generics.UpdateAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = UserSerializer
+    lookup_field = 'email'
+    queryset = User.objects.all()
+
+    def get_object(self):
+        try:
+            instance = self.queryset.get(email=self.request.user)
+            return instance
+        except User.DoesNotExist:
+            raise Http404
+
+# ユーザ削除のView(DELETE)
+class AuthInfoDeleteView(generics.DestroyAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = UserSerializer
+    lookup_field = 'email'
+    queryset = User.objects.all()
+
+    def get_object(self):
+        try:
+            instance = self.queryset.get(email=self.request.user)
+            return instance
+        except User.DoesNotExist:
+            raise Http404
